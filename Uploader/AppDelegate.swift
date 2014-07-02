0b8f32c625a779a3fdda921528d0ba444694cf5c
Masayuki Ono <mono0926@gmail.com>
2014-07-03 05:55:05 +0900

    つづき

--- Uploader/AppDelegate.swift
+++ Uploader/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
-        MagicalRecord.setupCoreDataStack()
+        
         
         Parse.setApplicationId(parseApplicationId, clientKey: parseClientKey)
         PFFacebookUtils.initializeFacebook()
--- Uploader/Container.swift
+++ Uploader/Container.swift
@@ -9,5 +9,20 @@
 import Foundation
 
 class Container {
+    // あとで https://github.com/hpique/SwiftSingleton
+    var _managedObjectContext:NSManagedObjectContext?
     
+    init() {
+    }
+    
+    var managedObjectContext:NSManagedObjectContext {
+        get  {
+            if let moc = _managedObjectContext? {
+                return moc
+            }
+            MagicalRecord.setupCoreDataStack()
+            _managedObjectContext = NSManagedObjectContext.MR_defaultContext()
+            return _managedObjectContext!
+        }
+    }
 }
